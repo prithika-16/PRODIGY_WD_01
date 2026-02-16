09749616dc67ff37d434d445bc3f5d3b5356d476
prithika-16 <prithikajayakumar16@gmail.com>
2026-02-16 18:48:51 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("scroll", function() {
-    let navbar = document.getElementById("navbar");
-    if (window.scrollY > 50) {
-        navbar.classList.add("scrolled");
-    } else {
-        navbar.classList.remove("scrolled");
-    }
-});
